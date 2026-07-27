ba6e8214e2f5e303ae83c06f981ad06ef89ff84d
George <georgemullarm045@gmail.com>
2026-07-27 11:40:25 +0530

    Refactor email transporter to use SMTP configuration
    Updated email transporter configuration to use SMTP settings.

--- utils/sendEmails.js
+++ utils/sendEmails.js
@@ -1,12 +1,15 @@
 const nodemailer = require('nodemailer')
 require('dotenv').config()
 
-const transporter =nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:process.env.EMAIL,
-        pass:process.env.PASS
-    }
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
 });
 
 async function verify(){
