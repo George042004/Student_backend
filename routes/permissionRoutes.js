a3812f382452c37843edaeaa3961ddb4b583f1b1
George042004 <georgemullarm045@gmail.com>
2026-08-12 19:33:38 +0530

    Added authentication middleware to permission routes

--- routes/permissionRoutes.js
+++ routes/permissionRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = require('express').Router();
 const permissionControllers = require('../controllers/permissionControllers')
+const auth = require('../middleware/auth')
 
 router.post('/',auth,permissionControllers.permission)
 router.get('/getRequests',permissionControllers.getRequests)
