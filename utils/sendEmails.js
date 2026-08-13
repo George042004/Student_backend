e54ea4e7ed55116545ca2babdeb6824b9a9b3c22
George042004 <georgemullarm045@gmail.com>
2026-08-13 14:36:03 +0530

    new changes done

--- utils/sendEmails.js
+++ utils/sendEmails.js
@@ -1,11 +1,6 @@
 const {Resend} = require('resend')
 require('dotenv').config()
 
-
-
-const {Resend} = require('resend')
-require('dotenv').config()
-
 const resend = new Resend(process.env.API_KEY)
 
 module.exports = resend
