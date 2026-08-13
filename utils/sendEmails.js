63fd49e9a47e8c1cf099b0a07206098c864c8463
George042004 <georgemullarm045@gmail.com>
2026-08-13 12:53:41 +0530

    Increase timeout settings for email transporter to enhance reliability

--- utils/sendEmails.js
+++ utils/sendEmails.js
@@ -8,6 +8,9 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  connectionTimeout:10000,
+  greetingTimeout:10000,
+  socketTimeout:10000,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASS,
