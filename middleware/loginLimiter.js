5db89b9a23ca4ac2def537ca716e9b6396a32cd1
George042004 <georgemullarm045@gmail.com>
2026-08-19 20:13:31 +0530

    refactor login rate limiter to use handler for custom response message

--- middleware/loginLimiter.js
+++ middleware/loginLimiter.js
@@ -4,8 +4,10 @@ const rateLimit = require('express-rate-limit')
 const Limiter = rateLimit({
     windowMs:15*60*1000,
     max:5,
-    message:{
-        message:"Too many login attempts. Please try again later."
+    handler:(req,res)=>{
+        res.status(429).json({
+            message:"Too many login attempts. Please try again later."
+        })
     }
 })
 
